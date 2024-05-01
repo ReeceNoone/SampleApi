37e8a4740c92361899d4941b302284ec48f90c8a
Reece Noone <reece.noone@outlook.com>
2024-05-01 13:26:48 +0100

    Updated event names

--- src/Locator.Contracts/Events/UsersEvents.cs
+++ src/Locator.Contracts/Events/UsersEvents.cs
@@ -2,7 +2,7 @@ namespace Locator.Contracts.Events;
 
 public class UsersEvents
 {
-    public string UserCreated { get; } = "UserCreated";
+    public string UserCreated { get; } = "User.Created";
 
-    public string UserUpdated { get; } = "UserUpdated";
+    public string UserUpdated { get; } = "User.Updated";
 }
